c0c4144b87ea4e856e28ae633cf8366cf199ea83
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2022-02-08 10:37:39 +0900

    Refactor: lombok 라이브러리로 @Data 어노테이션 적용

--- src/main/java/com/comento/project/board/BoardVO.java
+++ src/main/java/com/comento/project/board/BoardVO.java
@@ -2,6 +2,9 @@ package com.comento.project.board;
 
 import java.util.Date;
 
+import lombok.Data;
+
+@Data
 public class BoardVO {
 
 	private Integer no;
@@ -11,48 +14,5 @@ public class BoardVO {
 	private String password;
 	private Date regDate;
 	private int hit;
-	
-	public Integer getNo() {
-		return no;
-	}
-	public void setNo(Integer no) {
-		this.no = no;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public Date getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
-	public int getHit() {
-		return hit;
-	}
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
 	
 }
